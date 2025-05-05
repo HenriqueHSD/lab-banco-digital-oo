5ade961741cef342b90ea2dca8aff71f6b8975bc
Henrique Damião <henriquedamiao1709@gmail.com>
2025-05-05 15:41:48 -0300

    atualizacao da conta mostra simplificado e adicionar dados

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,31 @@
+import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Cliente venilton = new Cliente();
-		venilton.setNome("Venilton");
-		
-		Conta cc = new ContaCorrente(venilton);
-		Conta poupanca = new ContaPoupanca(venilton);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Cliente cliente = new Cliente();
 
-		cc.depositar(100);
-		cc.transferir(100, poupanca);
-		
-		cc.imprimirExtrato();
-		poupanca.imprimirExtrato();
-	}
+        System.out.println("Digite o nome do cliente: ");
+        String nome = sc.next();
+
+        cliente.setNome(nome);
+
+        Conta cc = new ContaCorrente(cliente);
+        Conta poupanca = new ContaPoupanca(cliente);
+
+        System.out.println("Digite o valor do depositor: ");
+        double depositor = sc.nextDouble();
+        cc.depositar(depositor);
+
+        System.out.println("Digite o valor do transferencia: ");
+        double transferir = sc.nextDouble();
+        cc.transferir(transferir, poupanca);
+
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+
+        sc.close();
+    }
 
 }
